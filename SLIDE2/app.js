b3e4ccc189b2b72adc0e77c6f7cbf6ac11c2f75f
66tuna <66tuna1018@gmail.com>
2021-08-10 15:11:30 +0900

    slide

---
+++ SLIDE2/app.js
@@ -0,0 +1,61 @@
+window.onload = function() {
+    let current = 0;
+    let isSlide = false;
+
+    function slide(target, dir) {
+        if (isSlide) return;
+
+        if (target <= 0) {
+            $(".slide-image").eq(target).css({ "left": `${dir * -100}` });
+            target += 7;
+        }
+
+        if (target >= 6) {
+            $(".slide-image").eq(target).css({ "right": `${dir * -100}` });
+            target -= 7;
+        }
+
+        console.log(target, dir);
+
+        isSlide = true;
+
+        $(".slide-image")
+            .eq(target)
+            .css({ "left": `${dir * 100}%` })
+            .animate({ "left": 0 }, 800);
+
+        $(".slide-image")
+            .eq(current)
+            .animate({ "left": `${-100 * dir}%` }, 800, function() {
+                isSlide = false;
+            });
+        current = target;
+
+        $(".pin").removeClass("active");
+        $(".pin").eq(target).addClass("active");
+    }
+
+    setInterval(function() {
+
+        slide(current + 1, 1);
+
+    }, 5000);
+
+    $(".slide-image").css({ "left": "100%" });
+    $(".slide-image").eq(current).css("left", 0);
+
+    $(".pin").on("click", function() {
+        let idx = $(this).index();
+        slide(idx, idx - current < 0 ? -1 : 1);
+
+        $(".pin").removeClass("active");
+        $(".pin").eq(idx).addClass("active");
+    });
+
+    $(".slide-btn").on("click", function() {
+        let dir = $(this).data("dir") * 1; //숫자로 강제 형변환
+        slide(current + dir, dir);
+    });
+
+
+};
